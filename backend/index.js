ad9bdc22fcd6a343eef98ae1b743e4ef7e550254
Rajesh97266 <rajesh97266@gmail.com>
2025-01-20 17:10:07 +0530

    userRoute has been created

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
+//routes
+
+import userRoutes from "./routes/userRoutes.js"
+
 //utils
 
 import connectDB from "./config/db.js"
@@ -20,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 
-app.use("api/users" , userRoutes)
+app.use("/api/users" , userRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
